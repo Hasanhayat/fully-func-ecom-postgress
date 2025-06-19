c831367d08a10886d4faa942b7339e652528be91
hasanhayat <aylskhsmvz.v@gmail.com>
2025-06-19 22:57:02 +0500

    login

--- server.mjs
+++ server.mjs
@@ -1,14 +1,102 @@
-import express from 'express';
-import db from './db.js';
-import cors from 'cors';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import db from "./db.js";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+const JWT_SECRET = process.env.SECRET_TOKEN;
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the API');
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
 });
+
+app.post("/sign-up", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  email = email.toLowerCase();
+  try {
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const existingUser = await db.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    // Hash password
+    const salt = bcrypt.genSalySync(10);
+    const hashedPassword = bcrypt.hastSync(password, salt);
+    // new user
+    await db.query(
+      "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
+      [firstName, lastName, email, hashedPassword]
+    );
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error during sign-up:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("login", async (req, res) => {
+  const { email, password } = req.body;
+  email = email.toLowerCase();
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+  try {
+    const user = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.rows[0].password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    // Generate token
+    const token = jwt.sign(
+      {
+        id: user.rows[0].id,
+        email: user.rows[0].email,
+        firstName: user.rows[0].first_name,
+        lastName: user.rows[0].last_name,
+        user_role: user.rows[0].role || "4", // Default role if not set
+        iat: Date.now() / 1000,
+        exp: Date.now() / 1000 + 1000 * 60 * 60 * 24,
+      },
+      JWT_SECRET
+    );
+    res.cookie("Token", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 86400000, //1 day
+    });
+
+    res.json({ message: "Login successful", user: user.rows[0] });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
