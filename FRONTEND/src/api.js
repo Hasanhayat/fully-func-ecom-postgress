d2a7cce0aaa88bc9a316dbf1841c9ce08eaea17f
hasanhayat <aylskhsmvz.v@gmail.com>
2025-07-11 22:13:47 +0500

    static build

--- FRONTEND/src/api.js
+++ FRONTEND/src/api.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
+let baseURL = "";
+
+const hostname = window.location.hostname;
+const protocol = window.location.protocol;
+
+if (hostname === "buytech-lilac.vercel.app") {
+  baseURL = "https://fully-func-ecom-postgress.vercel.app/api/v1";
+} else if (protocol === "http:") {
+  baseURL = "http://localhost:3002/api/v1";
+} else if (protocol === "https:") {
+  baseURL = "/api/v1"; // relative path for same-origin requests
+}
+
 const api = axios.create({
-  baseURL: "https://fully-func-ecom-postgress.vercel.app/api/v1",
+  baseURL,
   withCredentials: true,
 });
+
 export default api;
